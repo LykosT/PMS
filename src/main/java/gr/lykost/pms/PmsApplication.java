2514f999aa65093a9fb0864611e235367d267e18
LykosT <lykourgos.top@gmail.com>
2025-12-06 00:18:18 +0200

    Initial Commit

---
+++ src/main/java/gr/lykost/pms/PmsApplication.java
@@ -0,0 +1,15 @@
+package gr.lykost.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class PmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsApplication.class, args);
+    }
+
+}
