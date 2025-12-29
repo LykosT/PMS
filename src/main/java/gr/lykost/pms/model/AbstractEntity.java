4c702290fb29ef32d4f61e0c47dddddf274163d1
LykosT <lykourgos.top@gmail.com>
2025-12-29 12:15:13 +0200

    feat: Update AbstractEntity to add uuid

--- src/main/java/gr/lykost/pms/model/AbstractEntity.java
+++ src/main/java/gr/lykost/pms/model/AbstractEntity.java
@@ -3,6 +3,7 @@ package gr.lykost.pms.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +13,14 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @MappedSuperclass
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public abstract class AbstractTimestamp implements Serializable {
+public abstract class AbstractEntity implements Serializable {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
@@ -29,5 +30,13 @@ public abstract class AbstractTimestamp implements Serializable {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
+    @Column(nullable = false, unique = true, updatable = false, length = 36)
+    private String uuid;
 
+    @PrePersist
+    protected void generateUuid() {
+        if (uuid == null) {
+            uuid = UUID.randomUUID().toString();
+        }
+    }
 }
