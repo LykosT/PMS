c89bf2fcfce6c6e5c3e8c85e5e1b5b7c8d33fa2d
LykosT <lykourgos.top@gmail.com>
2026-01-13 22:48:46 +0200

    feat: add UnauthorizedException

--- src/main/java/gr/lykost/pms/core/exceptions/UnauthorizedException.java
+++ src/main/java/gr/lykost/pms/core/exceptions/UnauthorizedException.java
@@ -1,7 +1,9 @@
 package gr.lykost.pms.core.exceptions;
 
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
+public class UnauthorizedException extends ResourceGenericException {
+    private static final String DEFAULT_CODE = "Unauthorized Access";
+
+    public UnauthorizedException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
     }
 }
